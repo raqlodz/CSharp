767b130a901e36469fe4bed6b95f192912316987
Slawomir Raczynski <slawomir.raczynski@wp.pl>
2018-02-24 17:10:48 +0100

    First work

---
+++ Practice/Reflection_1/Reflection_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Reflection_1
+{
+    class Program
+    {
+        private static int a = 5, b = 10, c = 20;
+
+        static void Main(string[] args)
+        {
+            test1();
+            test2();
+
+            Console.WriteLine("a + b + c = " + (a + b + c));
+            Console.WriteLine("Please enter the name of the variable that you wish to change:");
+            string varName = Console.ReadLine();
+            Type t = typeof(Program);
+            FieldInfo fieldInfo = t.GetField(varName, BindingFlags.NonPublic | BindingFlags.Static);
+            if (fieldInfo != null)
+            {
+                Console.WriteLine("The current value of " + fieldInfo.Name + " is " + fieldInfo.GetValue(null) + ". You may enter a new value now:");
+                string newValue = Console.ReadLine();
+                int newInt;
+                if (int.TryParse(newValue, out newInt))
+                {
+                    fieldInfo.SetValue(null, newInt);
+                    Console.WriteLine("a + b + c = " + (a + b + c));
+                }
+                Console.ReadKey();
+            }
+        }
+
+        static void test1()
+        {
+            string test = "test";
+            Console.WriteLine(test.GetType().FullName);
+            Console.WriteLine(typeof(Int32).FullName);
+            Console.ReadKey();
+        }
+
+        static void test2()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] assemblyTypes = assembly.GetTypes();
+
+            foreach (Type t in assemblyTypes)
+                Console.WriteLine(t.Name);
+            Console.ReadKey();
+        }
+    }
+
+    class DummyClass
+    {
+        //Just here to make the output a tad less boring :)
+    }
+}
---
+++ Practice/Reflection_2/Reflection_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_2
+{
+    class Program
+    {
+        public static int gwint = 1;
+        public static decimal decymetr = 2.6M;
+        public static string pstrong = "ala";
+        public static bool bulin = true;
+        public static int Propertas { get; set; }
+
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] assemblyTypes = assembly.GetTypes();
+
+            foreach (Type assemblyType in assemblyTypes)
+            {
+                Console.WriteLine(assemblyType.Name);
+
+                FieldInfo[] fields = assemblyType.GetFields();
+                foreach (FieldInfo field in fields)
+                    Console.WriteLine("Field Name = {0}, Field Type = {1}, Field Value = {2}, Field Attribures = {3}", 
+                        field.Name, field.FieldType, field.GetValue(null), field.Attributes);
+
+                PropertyInfo[] properties = assemblyType.GetProperties();
+                foreach (PropertyInfo property in properties)
+                    Console.WriteLine("Property Name = {0}, Property Type = {1}, Property Value = {2}, Property Attributes = {3}", 
+                        property.Name, property.PropertyType, property.GetValue(null), property.Attributes);
+
+                MethodInfo[] methods = assemblyType.GetMethods();
+                foreach (MethodInfo method in methods)
+                    Console.WriteLine("Method Name = {0}, Method ReturnType = {1}, Method Attributes = {2}", 
+                        method.Name, method.ReturnType, method.Attributes);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class DummyClass1
+    {
+        public static int inttest1 = 21;
+        public static double doubletest1;
+        public static string s;
+        public int test1()
+        {
+            return 0;
+        }
+    }
+
+    class DummyClass2
+    {
+        public static byte bytetest2 = 255;
+        public static int inttest2 = 44;
+        public bool test2a (int a)
+        {
+            return true;
+        }
+    }
+}
